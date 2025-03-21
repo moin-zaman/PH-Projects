918eec2e9664598934ca3616e4c3ed7250d1ceda
moin-zaman <moinkhan758abzx@gmail.com>
2025-03-21 19:58:49 +0600

    Added blog.html and NavigatePage js File

---
+++ Milestone 5/DOM-Quest-Build-and-Manipulate/js/navigatePage.js
@@ -0,0 +1,14 @@
+// Blog Button
+
+
+const homeNavBtn = document.getElementById('btn-home-nav');
+
+
+
+
+homeNavBtn.addEventListener('click', function(){
+
+    window.location.href = './index.html';
+
+});
+
