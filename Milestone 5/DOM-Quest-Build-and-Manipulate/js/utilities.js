2fbf04fc3ee5a92029c0925d9a7cef2b3277ded4
moin-zaman <moinkhan758abzx@gmail.com>
2025-03-21 17:35:46 +0600

    Added Function for creating and Setting the new Div in history section

--- Milestone 5/DOM-Quest-Build-and-Manipulate/js/utilities.js
+++ Milestone 5/DOM-Quest-Build-and-Manipulate/js/utilities.js
@@ -38,3 +38,29 @@ function getIdForFacilitiesByID(id)
 
     return idForFacilities;
 }
+
+
+// Create and Set History Div using following ID
+
+function getDonatedLocation(id, donateAmountNoakhali)
+{
+    const donatedLocationNoakhali = document.getElementById(id).innerText;
+
+        const createHistoryDiv = document.createElement('div');
+
+
+        createHistoryDiv.innerText = `${donateAmountNoakhali} Taka is ${donatedLocationNoakhali}`;
+
+        const donatedDate = new Date().toLocaleString('en-GB');
+        const p = document.createElement('h1');
+        p.innerText = donatedDate;
+
+
+        createHistoryDiv.appendChild(p);
+
+
+
+        createHistoryDiv.classList.add('max-w-screen-xl', 'mx-auto', 'box-border', 'border', 'border-1', 'bg-yellow-200', 'rounded-3xl', 'shadow-lg', 'p-5', 'mb-4');
+
+        return createHistoryDiv;
+}
